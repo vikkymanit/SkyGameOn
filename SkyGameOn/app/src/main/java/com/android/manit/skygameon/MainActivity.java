80af23f67c74acb5027489215253b82cc168a3b9
Manit Kumar <vikky_manit@yahoo.co.in>
2016-11-20 03:09:51 +0100

    Auto minimize app

--- SkyGameOn/app/src/main/java/com/android/manit/skygameon/MainActivity.java
+++ SkyGameOn/app/src/main/java/com/android/manit/skygameon/MainActivity.java
@@ -3,6 +3,7 @@ package com.android.manit.skygameon;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,6 +22,10 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         listenAsync = new ListenAsync(MainActivity.this);
         listenAsync.execute();
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
     }
 
     @Override
